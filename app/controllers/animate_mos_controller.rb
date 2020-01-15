198475e5710908db8a57a7918a600c1891fb21e6
ckastner12 <github email address>
2020-01-14 17:55:04 -0800

    created putting and patching methods for the channel stream

--- app/controllers/animate_mos_controller.rb
+++ app/controllers/animate_mos_controller.rb
@@ -6,14 +6,19 @@ class AnimateMosController < ApplicationController
             serialized_data = ActiveModelSerializers::Adapter::Json.new(
                 AnimateMoSerializer.new(animate_mo)
             ).serializable_hash
-            ActionCable.server.broadcast("pictures_channel_#{picture.id}", serialized_data)
+            ActionCable.server.broadcast("pictures_channel_#{picture.id}", {method: "CHANNEL_POST", animation: serialized_data})
             render json: animate_mo
         end
     end
 
     def update
         animation = AnimateMo.find(params[:id])
+        picture = Picture.find(animation.picture_id)
         if animation.update(animate_mo_params)
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(
+                AnimateMoSerializer.new(animation)
+            ).serializable_hash
+            ActionCable.server.broadcast("pictures_channel_#{picture.id}", {method: "CHANNEL_PATCH", animation: serialized_data})
             render json: animation
         else
             render json: {error: "Invalid Update", message: ""}
