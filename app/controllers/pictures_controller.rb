030bad78be5718edca49507bba34acf0ab4e7a23
ckastner12 <github email address>
2020-01-21 10:04:48 -0800

    Patch canvas route working

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,6 +24,17 @@ class PicturesController < ApplicationController
         end
     end
 
+    def update
+        picture = Picture.find(params[:id])
+        if picture.update(picture_params)
+            serialized_data =  ActiveModelSerializers::Adapter::Json.new(
+                PictureSerializer.new(picture)
+            ).serializable_hash
+            ActionCable.server.broadcast("pictures_channel_#{picture.id}", {type: "PATCH_CANVAS", canvas: serialized_data})
+            render json: {message: "Patch Successful"}
+        end
+    end
+
     private
     def picture_params
         params.require(:picture).permit(:title, :user_id, :background)
